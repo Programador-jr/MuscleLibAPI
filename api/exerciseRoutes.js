fcd48f1d461bb8ff16e38d00faee4dc2439c509b
Daniel <70107167+Programador-jr@users.noreply.github.com>
2024-10-15 16:47:31 +0000

    feat

--- api/exerciseRoutes.js
+++ api/exerciseRoutes.js
@@ -11,12 +11,42 @@ router.get('/', async (req, res) => {
         const exercises = await Exercise.find()
             .skip(page * limit)
             .limit(limit);
-        
+
         res.json(exercises);
     } catch (err) {
         console.error('Erro ao buscar exercícios:', err);
         res.status(500).json({ message: 'Erro ao buscar os exercícios', error: err.message });
     }
 });
+
+router.get('/search', async (req, res) => {
+    const { query } = req.query;
+
+    try {
+        if (!query) {
+            return res.status(400).json({ message: 'Por favor, insira um termo de pesquisa' });
+        }
+
+        const suggestions = await Exercise.find({
+            $or: [
+                { name: { $regex: query, $options: 'i' } },
+                { equipment: { $regex: query, $options: 'i' } },
+                { primaryMuscles: { $regex: query, $options: 'i' } },
+                { secondaryMuscles: { $regex: query, $options: 'i' } }
+            ]
+        }).limit(5);
+
+        if (suggestions.length === 0) {
+            return res.json({ message: 'Exercício não encontrado', suggestions: [] });
+        }
+
+        const suggestionNames = suggestions.map(exercise => exercise.name);
+        res.json({ message: 'Acho que você quis dizer:', suggestions: suggestionNames });
+    } catch (error) {
+        console.error('Erro ao buscar sugestões de exercícios:', error);
+        res.status(500).json({ error: 'Erro ao buscar sugestões' });
+    }
+});
+
 
 module.exports = router;
